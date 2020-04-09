a96fcf63541e870ae3832ce1345d0721c3677bf9
Edward Sokolovsky <ricoshet47@gmail.com>
2020-04-09 10:02:24 +0300

    Initial Commit

---
+++ Coronavirus.INFO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coronavirus.INFO
+//
+//  Created by Edward on 09.04.2020.
+//  Copyright © 2020 Edward'S. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
